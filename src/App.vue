3934021b5b06e654935d3f3518cdbd8a7de75fdc
longplayer <bartaber@hotmail.com>
2022-02-25 17:11:01 +0100

    Removed demo components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <the-navigation />
+    <router-view :key="routeName" />
   </div>
-  <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import '@/assets/styles/index.scss'
+import { computed, onMounted } from 'vue'
+import { useRoute  } from 'vue-router'
+import TheNavigation from './components/TheNavigation.vue'
+export default {
+  components: { TheNavigation },
+  setup() {
+    const route = useRoute()
+    const routeName = computed(() => route.name?.toLowerCase())
+    return { routeName }
+  },
 }
+</script>
 
+<style lang="scss">
 #nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="nav">
+    <router-link :to="{ path: '/' }"> 
+      Home
+    </router-link> |
+    <router-link :to="{ path: '/characters' }">
+      See All Characters
+    </router-link>
+  </div>
+</template>
