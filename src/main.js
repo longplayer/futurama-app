d7ebddab5f328d44ab2bb47ad82ba9eb11e6d6b8
longplayer <bartaber@hotmail.com>
2022-02-26 16:36:16 +0100

    Fixed helper folder location

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import filters from '../helpers/filters'
+import filters from './helpers/filters'
 import App from './App.vue'
 import router from './router'
 import store from './store'
